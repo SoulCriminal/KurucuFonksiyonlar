86decbd0d934824400de1ad1f02bd8f9a5bd1615
HAKKI AYMAN <82755715+SoulCriminal@users.noreply.github.com>
2022-03-16 16:36:41 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+Calisan calisan3 = new Calisan("Lina","Ayman",123412412,"IT");
+
+calisan3.CalisanBilgileri();
+
+class Calisan
+{
+    public string Ad;
+    public string Soyad;
+    public int No;
+    public string Departman;
+
+    // Kurucu Fonksiyon oluşturarak class çağırılırken değer alması sağlanabilir
+    // daha okunaklı kod yazmaya olanak sağlar.
+
+    public Calisan(string ad, string soyad , int no , string departman)
+    {
+        Ad = ad;
+        Soyad = soyad;
+        No = no;
+        Departman = departman;
+    }
+
+    public void CalisanBilgileri()
+    {
+        Console.WriteLine("Çalışanın Adı:{0}",Ad);
+        Console.WriteLine("Çalışanın Soyadı:{0}",Soyad);
+        Console.WriteLine("Çalışanın Numarası:{0}",No);
+        Console.WriteLine("Çalışanın Departmanı:{0}",Departman);
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Merhabalar C# Kurucu Fonksiyonlar , kullanım şekilleri ve  açıklamaları içeren kodlarım!
+
+Merhabalar C# ile ilgili öğrenirken hazırladığım Kurucu Fonksiyon kullanımı ile ilgili kodlarımı içeren dosya. 
+
+
+# İçerik
+
+- Erişim Belirteçleri
+- Örnek tanımlanan sınıflar
+
